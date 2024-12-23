38642f88dec5579d16f0ddfe3ad905047d7fd4d8
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2024-12-23 11:34:39 +0300

    finish styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InfoSectionComponent } from './info-section/info-section.component';
+import { SubtitleComponent } from './subtitle/subtitle.component';
 import { TextComponent } from './text/text.component';
 import { TitleComponent } from './title/title.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TitleComponent, NgOptimizedImage, TextComponent],
+  imports: [
+    RouterOutlet,
+    TitleComponent,
+    NgOptimizedImage,
+    TextComponent,
+    InfoSectionComponent,
+    SubtitleComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/info-section/info-section.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './info-section.component.html',
+  styleUrl: './info-section.component.css',
+})
+export class InfoSectionComponent {
+  @Input() title: string = '';
+}
